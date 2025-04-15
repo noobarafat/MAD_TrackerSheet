bdf028a16c8a408ec0a9f36c44e3b7a0af894c34
noobarafat <kmarafatislam@gmail.com>
2025-04-15 23:00:43 +0600

    P17: Setting Screen with a toggle

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,65 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 void main() {
-  runApp(const AnimatedShapeApp());
+  runApp(const MyApp());
 }
 
-class AnimatedShapeApp extends StatelessWidget {
-  const AnimatedShapeApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Animated Container Demo',
-      debugShowCheckedModeBanner: false,
-      home: const AnimatedShapeScreen(),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class AnimatedShapeScreen extends StatefulWidget {
-  const AnimatedShapeScreen({super.key});
+class _MyAppState extends State<MyApp> {
+  bool isDarkMode = false;
 
-  @override
-  State<AnimatedShapeScreen> createState() => _AnimatedShapeScreenState();
-}
-
-class _AnimatedShapeScreenState extends State<AnimatedShapeScreen> {
-  double _width = 150;
-  double _height = 150;
-  Color _color = Colors.blue;
-  BorderRadiusGeometry _borderRadius = BorderRadius.circular(20);
-
-  void _changeShape() {
+  void toggleDarkMode(bool value) {
     setState(() {
-      final random = Random();
-      _width = random.nextInt(200).toDouble() + 100;
-      _height = random.nextInt(200).toDouble() + 100;
-      _color = Color.fromRGBO(
-        random.nextInt(256),
-        random.nextInt(256),
-        random.nextInt(256),
-        1,
-      );
-      _borderRadius = BorderRadius.circular(random.nextInt(100).toDouble());
+      isDarkMode = value;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Settings Example',
+      debugShowCheckedModeBanner: false,
+      theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      home: SettingsScreen(
+        isDarkMode: isDarkMode,
+        onToggle: toggleDarkMode,
+      ),
+    );
+  }
+}
+
+class SettingsScreen extends StatelessWidget {
+  final bool isDarkMode;
+  final Function(bool) onToggle;
+
+  const SettingsScreen({
+    super.key,
+    required this.isDarkMode,
+    required this.onToggle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Animated Container Example'),
+        title: const Text("Settings"),
         backgroundColor: Colors.deepPurple,
       ),
-      body: Center(
-        child: AnimatedContainer(
-          width: _width,
-          height: _height,
-          decoration: BoxDecoration(
-            color: _color,
-            borderRadius: _borderRadius,
+      body: ListView(
+        children: [
+          SwitchListTile(
+            title: const Text("Dark Mode"),
+            value: isDarkMode,
+            activeColor: Colors.deepPurple,
+            onChanged: onToggle,
           ),
-          duration: const Duration(milliseconds: 800),
-          curve: Curves.easeInOut,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _changeShape,
-        backgroundColor: Colors.deepPurple,
-        child: const Icon(Icons.play_arrow),
+        ],
       ),
     );
   }
